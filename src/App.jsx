3814593f9df3cd45ec94f969a79758ea1d0fac9e
Shaini18 <shainimohanty2003@gmail.com>
2024-09-04 16:44:12 +0530

    today

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import Navbar from './components/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return(
     <>
+    <div className='m-8 overflow-x-hidden text-neutral-300  selection:bg-cyan-900 '>
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    <div className='fixed top-0 z-[2] h-screen wscreen'></div>
+      <div className='container mx-auto px-8'>
     <Navbar></Navbar>
-
+    
+    </div>
+    </div>
      </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <>
-        <nav className=''></nav>
+        <nav className=' mb-20 flex items-center justify-between py-6'>
+          <div className='flex flex-shrink-0 items-center'>
+            <p className='text text-4xl mx-2 w-10'> abc</p>
+
+          </div>
+          <div className='m-8 items-center justify-center gap-4 text-2xl flex'>
+          <FaGithub />
+          <BsLinkedin />
+          </div>
+          </nav>
     </>
   )
 }
